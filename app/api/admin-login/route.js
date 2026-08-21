3f228277ab067ec4fc3cdfa1222aab9c07c07e29
SithubShabihaz <shabihazsite@gmail.com>
2026-08-21 19:53:58 +0500

    Simplify admin login by removing CAPTCHA verification
    Removed Turnstile verification and adjusted logic for admin login.

--- app/api/admin-login/route.js
+++ app/api/admin-login/route.js
@@ -1,32 +1,40 @@
 import { NextResponse } from 'next/server';
-import { verifyTurnstile } from 'nextjs-turnstile';
+
+
 
 export async function POST(request) {
+
   try {
-    const { email, password, token } = await request.json();
 
-    // 1. Verify Turnstile Token on the server
-    const isHuman = await verifyTurnstile(token, {
-      secretKey: process.env.TURNSTILE_SECRET_KEY,
-    });
+    const { email, password } = await request.json();
 
-    if (!isHuman) {
-      return NextResponse.json(
-        { success: false, message: 'Bot verification failed. Invalid CAPTCHA.' },
-        { status: 400 }
-      );
-    }
 
-    // 2. Check environment credentials
+
+    // .env file se email aur password uthana
+
     const adminEmail = process.env.ADMIN_EMAIL;
+
     const adminPassword = process.env.ADMIN_PASSWORD;
 
+
+
+    // Match karna
+
     if (email === adminEmail && password === adminPassword) {
+
       return NextResponse.json({ success: true, message: 'Login Successful!' }, { status: 200 });
+
     } else {
+
       return NextResponse.json({ success: false, message: 'Invalid Credentials! Wrong Email & Password.' }, { status: 401 });
+
     }
+
   } catch (error) {
+
     return NextResponse.json({ success: false, message: 'Server Error' }, { status: 500 });
+
   }
-}
+
+} 
+
